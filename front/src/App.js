a6bcf363ea54de1d0b29fd2573905fab0f5fda74
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-05-18 15:20:34 +0200

    feat(home): update home route

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,11 @@
 import "@/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
 import Home from "@/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
       <Toaster position="top-center" richColors />
     </div>
   );
