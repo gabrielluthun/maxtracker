004fcc9c786978423faeada79b3b18a7a58655c6
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-05-18 18:54:18 +0200

    refactor(back): re-export API schemas

---
+++ back/app/schemas/__init__.py
@@ -0,0 +1,15 @@
+from app.schemas.trips import (
+    DestinationGroup,
+    SearchResponse,
+    StationSuggestion,
+    SyncInfo,
+    TripOut,
+)
+
+__all__ = [
+    "TripOut",
+    "DestinationGroup",
+    "SearchResponse",
+    "SyncInfo",
+    "StationSuggestion",
+]
